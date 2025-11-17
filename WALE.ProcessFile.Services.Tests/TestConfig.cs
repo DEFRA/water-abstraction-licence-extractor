a3505f7b56b9e4908625c2d36fa16c62c9bf3212
Ryan Barlow <ryan.barlow@environment-agency.gov.uk>
2025-11-17 08:24:18 +0000

    639062 - Added tests

--- WALE.ProcessFile.Services.Tests/TestConfig.cs
+++ WALE.ProcessFile.Services.Tests/TestConfig.cs
@@ -56,6 +56,22 @@ public static class TestConfig
         }
     }
     
+    private static string? _pdfFolder3;
+
+    public static string PdfFolder3
+    {
+        get
+        {
+            if (_pdfFolder3 != null)
+            {
+                return _pdfFolder3;
+            }
+            
+            _pdfFolder3 = Config["PdfFolder3"]!;
+            return _pdfFolder3;
+        }
+    }
+    
     private static string? _aiVisionEndpoint;
 
     public static string AiVisionEndpoint

345250bedf2f1f0c06b737635881ac06fdb8fabd
Ryan Barlow <ryan.barlow@environment-agency.gov.uk>
2025-11-28 15:30:41 +0000

    Some more tests

--- WALE.ProcessFile.Services.Tests/IntegrationTests/AwsTextractOcrPdfTests.cs
+++ WALE.ProcessFile.Services.Tests/IntegrationTests/AwsTextractOcrPdfTests.cs
@@ -167,6 +167,24 @@ public class AwsTextractOcrPdfTests
         Assert.Equal("cubic metres", perYearUnits2);
 
         // See notes RE licence
+        
+        var agreedSchemaLicenceGroup = (await SchemaConverter.ToLicenceSetsAsync(
+            resultFull,
+            [],
+            [],
+            [],
+            [],
+            _pdfDataExtractor1,
+            TestConfig.PdfFolder,
+            0)).Last();
+
+        var agreedSchemaLicence = agreedSchemaLicenceGroup.Licences.First();
+        Assert.Single(agreedSchemaLicence.LinkedLicences);
+
+        Assert.Equal("14/46/03/0852", agreedSchemaLicence.LinkedLicences[0].LicenceNumber);
+        Assert.Single(agreedSchemaLicence.LinkedLicences[0].ContainedIn!);
+        Assert.Equal("AbstractionLimits", agreedSchemaLicence.LinkedLicences[0].ContainedIn![0].SectionName);
+        Assert.Equal("AggregateCondition", agreedSchemaLicence.LinkedLicences[0].ContainedIn![0].LinkReason);
     }
     
     [Theory]
@@ -199,6 +217,7 @@ public class AwsTextractOcrPdfTests
             0);
 
         var licence = schemaData[0].Licences[0];
+        Assert.Empty(licence.LinkedLicences);
 
         Assert.NotNull(licence.LicenceVersion.IssueDate);
         Assert.Equal(expectedIssueDate2, licence.LicenceVersion.IssueDate!.Value.ToShortDateString());

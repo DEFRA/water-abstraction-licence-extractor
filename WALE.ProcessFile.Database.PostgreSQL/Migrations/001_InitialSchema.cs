68d3370c2e5c4f8b78bd46d903014776ca95e289
James Bayliss Dawson <james.baylissdawson@environment-agency.gov.uk>
2025-12-10 16:58:10 +0000

    postgres schema migrated from mssql and with migration runner tool

---
+++ WALE.ProcessFile.Database.PostgreSQL/Migrations/001_InitialSchema.cs
@@ -0,0 +1,5 @@
+namespace WALE.ProcessFile.Database.PostgreSQL.Migrations;
+
+public class 001_InitialSchema {
+    
+}
---
+++ WALE.ProcessFile.Database.PostgreSQL/Services/PostgresReadService.cs
@@ -0,0 +1,6 @@
+namespace WALE.ProcessFile.Database.PostgreSQL.Services;
+
+public class PostgresReadService
+{
+    
+}

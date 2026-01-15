89648879d2c3708317c314229549b6c2ab9c1bf1
Ryan Barlow <ryan.barlow@environment-agency.gov.uk>
2026-01-15 16:33:07 +0000

    Added a useful comment

--- WALE.ProcessFile.Services.Tests/IntegrationTests/TesseractAndAwsTextractOcrPdfTests.cs
+++ WALE.ProcessFile.Services.Tests/IntegrationTests/TesseractAndAwsTextractOcrPdfTests.cs
@@ -12,6 +12,7 @@ using MatchType = WALE.ProcessFile.Core.Enums.MatchType;
 
 namespace WALE.ProcessFile.Services.Tests.IntegrationTests;
 
+// These tests are slow as we are limited to one scan per second from AWS Textract by default
 [Collection("AWS Textract")]
 public class TesseractAndAwsTextractOcrPdfTests(SingletonAwsTextractFixture textractFixture)
     : IClassFixture<SingletonAwsTextractFixture>

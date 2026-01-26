883dc99ad9f5de865979cd0b2d68c4ef7790e1da
James Bayliss Dawson <james.baylissdawson@environment-agency.gov.uk>
2026-01-26 14:45:49 +0000

    fix merge problem

--- WALE.ProcessFile.Services/Configuration/LabelConfiguration.cs
+++ WALE.ProcessFile.Services/Configuration/LabelConfiguration.cs
@@ -2460,7 +2460,7 @@ public static class LabelConfiguration
     private static readonly TextToMatch EnvironmentAgencyTelephone1Pattern =
         new("708 506 506"); // Only this bit matches the pattern (excludes first number)
     private static readonly TextToMatch EnvironmentAgencyTelephone2Pattern =
-        new("80 70 60"); // Only this bit matches the pattern (excludes first number)
+        new("800 80 70 60"); // Only this bit matches the pattern (excludes first number)
     private static readonly TextToMatch EnvironmentAgencyTelephone3Pattern =
         new("345 988 1188"); // Only this bit matches the pattern (excludes first number)
     private static readonly TextToMatch EnvironmentAgencyTelephone4Pattern =

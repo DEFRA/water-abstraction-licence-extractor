29870894f125e5d5ecd0494a5993b4e0f039944a
James Bayliss Dawson <james.baylissdawson@environment-agency.gov.uk>
2026-01-27 10:44:37 +0000

    final test updates

--- WALE.ProcessFile.Services.Tests/IntegrationTests/TesseractAndAzureAiVisionOcrPdfTests.cs
+++ WALE.ProcessFile.Services.Tests/IntegrationTests/TesseractAndAzureAiVisionOcrPdfTests.cs
@@ -376,10 +376,10 @@ public class TesseractAndAzureAiVisionOcrPdfTests
     [InlineData("22702013__2-27-02-013 6999981.PDF", "16 June 2000", "16/06/2000", 13, 0, "2/27/02/013")] // Correct
     [InlineData("22632370__2-26-32-370 6937616.PDF", "9 February 2004", "09/02/2004", 14, 1, "2/26/32/370")] // Correct
     [InlineData("22706035__2-27-06-035 6957806.PDF", "9 FEBRUARY 2004", "09/02/2004", 14, 0, "2/27/06/035")] // Correct
-    [InlineData("22707039__Application New Licence Issued - [21.01.2008] - (21.01.2008).PDF", "0 1 OCT 2002", "01/10/2002", 12, 1, "2/27/07/039")] // Correct // TOOD - Fix a bug where it thinks a linked licence number when its actually a 1 and a slash mixed up
+    [InlineData("22707039__Application New Licence Issued - [21.01.2008] - (21.01.2008).PDF", "0 1 OCT 2002", "01/10/2002", 12, 0, "2/27/07/039")] // Correct
     [InlineData("12506023__Application type unknown Licence Issued (26.01.2006).PDF", "26 JAN 2050", "26/01/2050", 13, 0, "1/25/06/023")] // Year incorrect - faint stamp, can't even read as a human
-    [InlineData("22634080__Non-Application Licence Document (27.03.1997).PDF", "27 MAR 1997", "27/03/1997", 12, 0, "2/26/34/080")] // Correct
-    [InlineData("22709167__Non-Application Licence Document (27.03.1997).PDF", "2.7. MAR.1897", "27/03/1897", 12, 0, "2/27/09/167")] // Incorrect - stamp is not amazing
+    [InlineData("22634080__Non-Application Licence Document (27.03.1997).PDF", "27 MAR 1997", "27/03/1997", 11, 0, "2/26/34/080")] // Correct
+    [InlineData("22709167__Non-Application Licence Document (27.03.1997).PDF", "2.7. MAR.1897", "27/03/1897", 11, 0, "2/27/09/167")] // Incorrect - stamp is not amazing
     [InlineData("22715238__Non-Application Licence Document (05.03.2004).PDF", "5 MAR 2004", "05/03/2004", 14, 0, "2/27/15/238")] // Correct (I think - there is '-' in the stamp)
     public async Task WhenHarishSpottedNoIssueDateFiles1_ThenIssueDateCorrectly(
         string filename,

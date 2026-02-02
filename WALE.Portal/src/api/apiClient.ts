d72d7334744f697786288598a91104f1b68c78d4
Ryan Barlow <ryan.barlow@environment-agency.gov.uk>
2026-02-02 13:58:25 +0000

    Set portal config to env variable

--- WALE.Portal/src/api/apiClient.ts
+++ WALE.Portal/src/api/apiClient.ts
@@ -1,6 +1,8 @@
 import {Client} from './generated/apiClient';
 
-export const waleApiBaseUrl = "http://localhost:8080";
+export let waleApiBaseUrl = import.meta.env.WALE_API_BASE_URL;
+if (!waleApiBaseUrl) waleApiBaseUrl = "http://localhost:8080";
+
 export const waleApiClient = new Client(waleApiBaseUrl);
 
 export default waleApiClient;

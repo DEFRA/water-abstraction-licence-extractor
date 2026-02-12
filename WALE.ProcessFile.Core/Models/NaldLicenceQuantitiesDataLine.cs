b5e62ef9defe1232f7c9f187875ee1f606f771ff
James Bayliss Dawson <james.baylissdawson@environment-agency.gov.uk>
2026-02-12 15:41:07 +0000

    clean up redundant line breaks

--- WALE.ProcessFile.Core/Models/NaldLicenceQuantitiesDataLine.cs
+++ WALE.ProcessFile.Core/Models/NaldLicenceQuantitiesDataLine.cs
@@ -3,24 +3,14 @@ namespace WALE.ProcessFile.Core.Models;
 public class NaldLicenceQuantitiesDataLine
 {
     public string LookupKey => $"{FgacRegionCode}|{AabvAablId}";
-    
     public int? Id { get; set; }
-
     public int? AabvAablId { get; set; }
-
     public int? AabvIssueNo { get; set; }
-
     public int? AabvIncrNo { get; set; }
-
     public string? MaxAnnualQty { get; set; }
-
     public string? MaxDailyQty { get; set; }
-
     public string? AggregatedInd { get; set; }
-
     public string? PurpPointsInd { get; set; }
-
     public string? UserValidInd { get; set; }
-
     public string? FgacRegionCode { get; set; }
 }
--- WALE.ProcessFile.Core/Models/NaldLicenceVersionDataLine.cs
+++ WALE.ProcessFile.Core/Models/NaldLicenceVersionDataLine.cs
@@ -3,52 +3,28 @@ namespace WALE.ProcessFile.Core.Models;
 public class NaldLicenceVersionDataLine
 {
     public string LookupKey => $"{FgacRegionCode}|{AablId}";
-    
     public int? AablId { get; set; }
-
     public int? IssueNo { get; set; }
-
     public int? IncrNo { get; set; }
-
     public string? AabvType { get; set; }
-
     public string? EffStDate { get; set; }
-
     public string? Status { get; set; }
-
     public string? ReturnsReq { get; set; }
-
     public string? Chargeable { get; set; }
-
     public string? AsrcCode { get; set; }
-
     public int? AconAparId { get; set; }
-
     public int? AconAaddId { get; set; }
-
     public string? AltyCode { get; set; }
-
     public string? AcclCode { get; set; }
-
     public string? MultipleLh { get; set; }
-
     public string? LicSigDate { get; set; }
-
     public string? AppNo { get; set; }
-
     public string? LicDocFlag { get; set; }
-
     public string? EffEndDate { get; set; }
-
     public string? ExpiryDate1 { get; set; }
-
     public string? WaAltyCode { get; set; }
-
     public string? VolConv { get; set; }
-
     public string? WrtCode { get; set; }
-
     public string? DeregCode { get; set; }
-
     public string? FgacRegionCode { get; set; }
 }

94e660a156f11a901feb40ec6c497cf634ff4ecb
James Bayliss Dawson <james.baylissdawson@environment-agency.gov.uk>
2026-04-03 12:55:13 +0100

    set created date, fix save error

--- WALE.ProcessFile.Database.PostgreSQL/Services/PostgresWriteService.cs
+++ WALE.ProcessFile.Database.PostgreSQL/Services/PostgresWriteService.cs
@@ -564,7 +564,7 @@ public class PostgresWriteService(INpgsqlDataSourceProvider dataSourceProvider)
         await using var connection = GetPostgresConnection();
         const string sql = """
                            INSERT INTO licence_section_verification (licence_file_id, process_run_id, licence_section_name, licence_section_value, verification_type, created_date_time_utc)
-                           VALUES (@LicenceFileId, @ProcessRunId, @LicenceSectionName, @LicenceSectionValue, @VerificationType, @CreatedDateTimeUtc)
+                           VALUES (@LicenceFileId, @ProcessRunId, @LicenceSectionName, CAST(@LicenceSectionValue AS jsonb), @VerificationType, @CreatedDateTimeUtc)
                            RETURNING licence_section_verification_id
                            """;
 
@@ -579,7 +579,7 @@ public class PostgresWriteService(INpgsqlDataSourceProvider dataSourceProvider)
                 verification.LicenceSectionName,
                 verification.LicenceSectionValue,
                 verification.VerificationType,
-                verification.CreatedDateTimeUtc
+                DateTime.UtcNow
             });
     }
 

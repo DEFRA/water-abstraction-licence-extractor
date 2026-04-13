9aef7736d18225349f1b33e6f8d906f913bce2de
James Bayliss Dawson <james.baylissdawson@environment-agency.gov.uk>
2026-04-13 07:53:28 +0100

    placeholder sub-tab

--- WALE.Portal/src/components/LicenceSectionVerification/VerificationContent.tsx
+++ WALE.Portal/src/components/LicenceSectionVerification/VerificationContent.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type {Licence} from "../../api/generated/apiClient.ts";
 import {LicenceSection} from "./LicenceSection";
 import {LinkedLicences} from "./LinkedLicences";
@@ -8,20 +9,59 @@ interface VerificationContentProps {
     onJumpToPage: (pageNumber: number) => void;
 }
 
+type SubTabType = 'verify' | 'history';
+
 export function VerificationContent({ licence, processRunId, onJumpToPage }: VerificationContentProps) {
+    const [activeSubTab, setActiveSubTab] = useState<SubTabType>('verify');
+
     return (
         <div id="properties" style={{ padding: '10px' }}>
-            <LicenceSection 
-                title="Linked Licences" 
-                licenceFileId={licence.dmsFileId!} 
-                processRunId={processRunId}
-            >
-                <LinkedLicences 
-                    licence={licence} 
-                    isEditing={false} // This will be overridden by LicenceSection's React.cloneElement
-                    onJumpToPage={onJumpToPage}
-                />
-            </LicenceSection>
+            <ul className="ul-links">
+                <li>
+                    <a
+                        href="#"
+                        className={activeSubTab === 'verify' ? 'selectedTab' : ''}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setActiveSubTab('verify');
+                        }}
+                    >
+                        Verify
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#"
+                        className={activeSubTab === 'history' ? 'selectedTab' : ''}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setActiveSubTab('history');
+                        }}
+                    >
+                        History
+                    </a>
+                </li>
+            </ul>
+
+            {activeSubTab === 'verify' && (
+                <LicenceSection 
+                    title="Linked Licences" 
+                    licenceFileId={licence.dmsFileId!} 
+                    processRunId={processRunId}
+                >
+                    <LinkedLicences 
+                        licence={licence} 
+                        isEditing={false} // This will be overridden by LicenceSection's React.cloneElement
+                        onJumpToPage={onJumpToPage}
+                    />
+                </LicenceSection>
+            )}
+
+            {activeSubTab === 'history' && (
+                <div>
+                    {/* History content will go here */}
+                </div>
+            )}
         </div>
     );
 }

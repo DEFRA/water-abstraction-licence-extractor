a89a42890048b0fdef92a3ec51274654f230a14d
James Bayliss Dawson <james.baylissdawson@environment-agency.gov.uk>
2026-05-05 08:08:51 +0100

    .

--- WALE.ProcessFile.Services/Services/LocalFileService.cs
+++ WALE.ProcessFile.Services/Services/LocalFileService.cs
@@ -38,6 +38,7 @@ public class LocalFileService(string folderPath) : IFileService
 
     public async Task UploadFileChunkAsync(string filename, Stream stream, int chunkIndex, int totalChunks)
     {
+        await Task.Delay(1000);
         var filePath = Path.Combine(FolderPath, filename);
         var mode = chunkIndex == 0 ? FileMode.Create : FileMode.Append;
         await using var fileStream = new FileStream(filePath, mode, FileAccess.Write, FileShare.None);

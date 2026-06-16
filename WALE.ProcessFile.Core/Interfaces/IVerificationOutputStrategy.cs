fbcd99663bb5212293870ece163b2bf2caa7f298
James Bayliss Dawson <james.baylissdawson@environment-agency.gov.uk>
2026-06-16 11:42:56 +0100

    Patch the LL list in grid with incoming links derived from the overrides (#152)
    * get the inverted verifications and pass to the strategy
    
    * patch the linked licences list with incoming

--- WALE.ProcessFile.Core/Interfaces/IVerificationOutputStrategy.cs
+++ WALE.ProcessFile.Core/Interfaces/IVerificationOutputStrategy.cs
@@ -5,5 +5,5 @@ namespace WALE.ProcessFile.Core.Interfaces;
 public interface IVerificationOutputStrategy
 {
     string SectionName { get; }
-    void HandleVerifications(IEnumerable<LicenceSectionVerification> verifications, OutputListDataItem listRow);
+    void HandleVerifications(IEnumerable<LicenceSectionVerification> verifications, OutputListDataItem listRow, IEnumerable<InvertedLicenceSectionVerification> invertedVerifications);
 }

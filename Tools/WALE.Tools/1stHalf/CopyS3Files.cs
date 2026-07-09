afee73b4079f72007c4789f64869e4e83f95ae0e
Ryan Barlow <ryan.barlow@environment-agency.gov.uk>
2026-07-09 09:54:56 +0100

    Some fixes and tools for file uploading

--- Tools/WALE.Tools/1stHalf/CopyS3Files.cs
+++ Tools/WALE.Tools/1stHalf/CopyS3Files.cs
@@ -1,3 +1,4 @@
+using WALE.ProcessFile.Services.Helpers;
 using WALE.ProcessFile.Services.Services;
 using WALE.Tools.Config;
 
@@ -7,24 +8,40 @@ public static class CopyS3Files
 {
     public static async Task RunAsync()
     {
-        var sourceHttpClient = new HttpClient();
-        sourceHttpClient.BaseAddress = new Uri(KeyConfig.ApiBaseUrl); // Source url
-        var sourceFileService = new ApiFileService(sourceHttpClient);
+        var sourceHttpClient = HttpHelper.GetResilientHttpClient(
+            KeyConfig.ApiBaseUrl,
+            100,
+            30);
         
-        var destinationHttpClient = new HttpClient();
-        destinationHttpClient.BaseAddress = new Uri("http://localhost:8080"); // Hardcoded destination url
+        var destinationHttpClient = HttpHelper.GetResilientHttpClient(
+            "http://localhost:8080",
+            100,
+            30);
+        
+        var sourceFileService = new ApiFileService(sourceHttpClient);
         var destinationFileService = new ApiFileService(destinationHttpClient);
 
-        var sourceFiles = await sourceFileService.GetAllFilesWithMetadataAsync(
-            string.Empty,
-            int.MaxValue);
+        var sourceFiles = await sourceFileService.GetAllFilesAsync();
+
+        // For debugging / limiting file uploads
+        /*sourceFiles = sourceFiles
+            //.Where(x => x == "02107__100e7d07-a1a2-43fb-9e69-890b97926f8d.pdf")
+            .Take(100)
+            .ToList();*/
         
         var copyingTasks = new List<Task>();
         const int maxConcurrent = 5;
+        var loopIdx = 1;
         
-        foreach (var filename in sourceFiles.Select(sourceFileMetadata => sourceFileMetadata.Filename))
+        foreach (var sourceFilename in sourceFiles)
         {
-            copyingTasks.Add(CopyFileAsync(filename, sourceFileService, destinationFileService));
+            copyingTasks.Add(
+                CopyFileAsync(
+                    sourceFilename,
+                    sourceFileService,
+                    destinationFileService,
+                    loopIdx++,
+                    sourceFiles.Count));
             
             if (copyingTasks.Count != maxConcurrent)
             {
@@ -44,18 +61,20 @@ public static class CopyS3Files
                     copyingTasks.Remove(toRemoveItem);
                 }
             }
-            
-            foreach (var copyingTask in copyingTasks)
-            {
-                await copyingTask;
-            }
+        }
+        
+        foreach (var copyingTask in copyingTasks)
+        {
+            await copyingTask;
         }
     }
 
     private static async Task CopyFileAsync(
         string filename,
         ApiFileService sourceFileService,
-        ApiFileService destinationFileService)
+        ApiFileService destinationFileService,
+        int loopIdx,
+        int totalFiles)
     {
         var sourceFileStream = await sourceFileService.GetFileAsStreamAsync(filename);
 
@@ -65,19 +84,28 @@ public static class CopyS3Files
         }
         
         const int chunkSize = 5 * 1024 * 1024; // 5MB
+        var isChunked = sourceFileStream.Length > chunkSize;
         
-        if (sourceFileStream.Length > chunkSize)
+        if (isChunked)
         {
             var chunkIndex = 0;
             var totalChunks = Convert.ToInt32(Math.Ceiling(sourceFileStream.Length / (double)chunkSize));
                 
             string? uploadId = null;
-                
+            var fullByteArray = GetByteArray(sourceFileStream);
+            
             while (chunkIndex < totalChunks)
             {
+                var chunkOfByteArray = fullByteArray
+                    .Skip(chunkIndex * chunkSize)
+                    .Take(chunkSize)
+                    .ToArray();
+                
+                var streamChunk = new MemoryStream(chunkOfByteArray);
+                
                 var tempUploadId = await destinationFileService.UploadFileChunkAsync(
                     filename,
-                    sourceFileStream,
+                    streamChunk,
                     chunkIndex++,
                     totalChunks,
                     uploadId);
@@ -89,5 +117,22 @@ public static class CopyS3Files
         {
             await destinationFileService.UploadFileAsStreamAsync(filename, sourceFileStream);
         }
+        
+        var additionalText = isChunked ? " (chunked)" : string.Empty;
+        Console.WriteLine($"Uploaded file {loopIdx} of {totalFiles} - {filename}{additionalText}");
+    }
+    
+    private static byte[] GetByteArray(Stream stream)
+    {
+        if (stream is MemoryStream memStream)
+        {
+            return memStream.ToArray();
+        }
+
+        using var memoryStream = new MemoryStream();
+        stream.CopyTo(memoryStream);
+        
+        var bytes = memoryStream.ToArray();
+        return bytes;
     }
 }

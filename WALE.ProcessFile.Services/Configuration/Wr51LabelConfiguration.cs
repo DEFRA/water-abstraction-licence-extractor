53dae02e7935cc2337638bb3464f3e4cd00c4dba
Ryan Barlow <ryan.barlow@environment-agency.gov.uk>
2026-07-15 11:16:22 +0100

    Added 2 more tests

--- WALE.ProcessFile.Services/Configuration/Wr51LabelConfiguration.cs
+++ WALE.ProcessFile.Services/Configuration/Wr51LabelConfiguration.cs
@@ -9,44 +9,54 @@ public static class Wr51LabelConfiguration
     {
         return
         [
-            ("SourceOfSupply", GetInOrderField("Source of supply:", "SourceOfSupply")),
-            ("PointOfAbstraction", GetInOrderField("Point of abstraction:", "PointOfAbstraction")),
-            ("MeansOfAbstraction", GetInOrderField("Means of abstraction:", "MeansOfAbstraction")),
-            ("Purposes", GetInOrderField("Purpose(s):", "Purposes")),
-            ("Period", GetInOrderField("Period:", "Period")),
-            ("Quantities", GetInOrderField("Quantities:", "Quantities")),
-            ("MeansOfMeasurement", GetInOrderField("Means of measurement:", "MeansOfMeasurement")),
-            ("Records", GetInOrderField("R ecords:", "Records")),
-            ("ProvisionOfInformation", GetInOrderField("Provision of information:", "ProvisionOfInformation")),
-            ("SpecialConditions", GetInOrderField("Special conditions:", "SpecialConditions")),
-            ("Land", GetInOrderField("Land (only if specified):", "Land")),
-            ("ChargingFactors", GetInOrderField("Charging factors:", "ChargingFactors")),
-            ("OtherProvisions", GetInOrderField("Other provisions (specify below):", "OtherProvisions")),
+            ("SourceOfSupply", GetInOrderField("Source of supply", "SourceOfSupply")),
+            ("PointOfAbstraction", GetInOrderField("Point of abstraction", "PointOfAbstraction")),
+            ("MeansOfAbstraction", GetInOrderField("Means of abstraction", "MeansOfAbstraction")),
+            ("Purposes", GetInOrderField("Purpose(s)", "Purposes")),
+            ("Period", GetInOrderField("Period", "Period")),
+            ("Quantities", GetInOrderField("Quantities", "Quantities")),
+            ("MeansOfMeasurement", GetInOrderField("Means of measurement", "MeansOfMeasurement")),
+            ("Records", GetInOrderField("R ecords", "Records")),
+            ("ProvisionOfInformation", GetInOrderField("Provision of information", "ProvisionOfInformation")),
+            ("SpecialConditions", GetInOrderField("Special conditions", "SpecialConditions")),
+            ("Land", GetInOrderField("Land (only if specified)", "Land")),
+            ("ChargingFactors", GetInOrderField("Charging factors", "ChargingFactors")),
+            ("OtherProvisions", GetInOrderField("Other provisions (specify below)", "OtherProvisions")),
             ("LicenceNumber", TextAfterLabel("Licence No. (or Application No. or GIC No. etc.)", "LicenceNumber", 1)),
-            ("MetWith", TextAfterLabel("Met with:", "MetWith", 0)),
-            ("InspectingOfficer", TextAfterLabel("Inspecting Officer:", "InspectingOfficer", 0)),
-            ("SiteAddress", TextAfterLabel("Site address (if different):", "SiteAddress", 1)),
-            ("InspectionClass", TextAfterLabel("Inspection Class:", "InspectionClass", 1)),
-            ("TelephoneNumber", TextAfterLabel("Telephone No:", "TelephoneNumber", 1)),
-            ("Position", TextAfterLabel("Position:", "Position", 0)),
-            ("Time", TextAfterLabel("Time:", "Time", 0)),
-            ("NameAndAddress", TextAfterLabel("Name and address:", "NameAndAddress", 2)),
-            ("MeterMake", TextAfterLabel("Meter make:", "MeterMake", 0)),
-            ("SerialNumber", TextAfterLabel("Serial number:", "SerialNumber", 0)),
-            ("Reading", TextAfterLabel("Reading:", "Reading", 0)),
-            ("Units", TextAfterLabel("Units:", "Units", 0)),
+            ("MetWith", TextAfterLabel("Met with", "MetWith", 0)),
+            ("InspectingOfficer", TextAfterLabel("Inspecting Officer", "InspectingOfficer", 0)),
+            ("SiteAddress", TextAfterLabel("Site address (if different)", "SiteAddress", 1)),
+            ("InspectionClass", TextAfterLabel("Inspection Class", "InspectionClass", 1)),
+            ("TelephoneNumber", TextAfterLabel("Telephone No", "TelephoneNumber", 1)),
+            ("Position", TextAfterLabel("Position", "Position", 0)),
+            ("Time", TextAfterLabel("Time", "Time", 0)),
+            ("NameAndAddress", TextToFindIsBetweenLabels(
+                "Name and address",
+                "Site address",
+                "NameAndAddress",
+                7,
+                LimitTo.SameColumn)),
+            ("MeterMake", TextAfterLabel("Meter make", "MeterMake", 0)),
+            ("SerialNumber", TextAfterLabel("Serial number", "SerialNumber", 0)),
+            ("Reading", TextAfterLabel("Reading", "Reading", 0)),
+            ("Units", TextAfterLabel("Units", "Units", 0)),
             ("Other", TextAfterLabel("Other:", "Other", 0)),
-            ("CertificatesOfRecords", TextAfterLabel("Certificates or records available for:", "CertificatesOfRecords", 0)),
+            ("CertificatesOfRecords", TextAfterLabel("Certificates or records available for", "CertificatesOfRecords", 0)),
             ("DateOfCertification", TextAfterLabel("Date of certificate or", "DateOfCertification", 1, [new("record:")])),
-            ("Calibration", TextAfterLabel("Calibration:", "Calibration", 1)),
-            ("Conformance", TextAfterLabel("Conformance:", "Conformance", 1)),
-            ("FlowVerification", TextAfterLabel("Flow verification:", "FlowVerification", 1)),
-            ("MeterVerification", TextAfterLabel("Meter verification:", "MeterVerification", 1)),
-            ("WhereKept", TextAfterLabel("Where kept:", "WhereKept", 0)),
-            ("FormSentTo", TextAfterLabel("Form sent to:", "FormSentTo", 0)),
+            ("Calibration", TextAfterLabel("Calibration", "Calibration", 1)),
+            ("Conformance", TextAfterLabel("Conformance", "Conformance", 1)),
+            ("FlowVerification", TextAfterLabel("Flow verification", "FlowVerification", 1)),
+            ("MeterVerification", TextAfterLabel("Meter verification", "MeterVerification", 1)),
+            ("WhereKept", TextAfterLabel("Where kept", "WhereKept", 0)),
+            ("FormSentTo", TextAfterLabel("Form sent to", "FormSentTo", 0)),
             ("Date", TextAfterLabel("Date:", "Date", 0)),
             ("DocumentTemplateVersion", TextAfterLabel("Document Template Version:", "DocumentTemplateVersion", 0)),
-            ("GeneralComments", GeneralComments()),
+            ("GeneralComments", TextToFindIsBetweenLabels(
+                "General comments, details / dates of occupation changes, actions required etc.",
+                "Form sent to",
+                "GeneralComments",
+                100,
+                LimitTo.WholeLine)),
             ("MaintenanceLine", MaintenanceLine("Maintenance:", "Readings taken", "MaintenanceLine")),
             ("ReadingsTakenLine", MaintenanceLine("Readings taken:", "Where Kept", "ReadingsTakenLine")),
             ("InspectionDate", TextAfterLabelWithSpecifiedColumn("Inspection Date:", "InspectionDate", 1, 1))
@@ -89,7 +99,12 @@ public static class Wr51LabelConfiguration
         ];
     }
     
-    private static List<LabelToMatch> GeneralComments()
+    private static List<LabelToMatch> TextToFindIsBetweenLabels(
+        string startText,
+        string endText,
+        string name,
+        int nextLines,
+        LimitTo limitTo)
     {
         return
         [
@@ -97,21 +112,25 @@ public static class Wr51LabelConfiguration
             {
                 TextStart =
                 [
-                    new("General comments, details / dates of occupation changes, actions required etc.")
+                    new(startText)
                     {
                         ColumnMustStartWith = true
                     }
                 ],
                 TextEnd = 
                 [
-                    new("Form sent to") { LineMustStartWith = true},
+                    new(endText) { LineMustStartWith = true},
                     new("[END_OF_BLOCK]")
                 ],
                 Position = LabelPosition.TextToFindIsBetweenLabels,
                 Format = "Text",
                 PreviousLinesToFetch = 0,
-                NextLinesToFetch = 100,
-                Name = "GeneralComments"
+                NextLinesToFetch = nextLines,
+                LimitTo = limitTo,
+                Name = name,
+                Remove = [
+                    new(startText) // TODO not sure why we have to add this, we dont always have to with betweens - probably because of the column limiting
+                ]
             }
         ];
     }

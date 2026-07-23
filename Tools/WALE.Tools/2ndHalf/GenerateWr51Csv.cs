72ec691bc973f22e9f5df363a9f47d89ea19517d
Ryan Barlow <ryan.barlow@environment-agency.gov.uk>
2026-07-23 17:15:26 +0100

    WIP

--- Tools/WALE.Tools/2ndHalf/GenerateWr51Csv.cs
+++ Tools/WALE.Tools/2ndHalf/GenerateWr51Csv.cs
@@ -13,6 +13,7 @@ using WALE.ProcessFile.Services.Configuration;
 using WALE.ProcessFile.Services.Converters;
 using WALE.ProcessFile.Services.Docnet;
 using WALE.ProcessFile.Services.Helpers;
+using WALE.ProcessFile.Services.Models.OutputSchema.Wr51;
 using WALE.ProcessFile.Services.Output;
 using WALE.ProcessFile.Services.PdfPig;
 using WALE.ProcessFile.Services.Services;
@@ -91,138 +92,60 @@ public static class GenerateWr51Csv
 
         const int processRunId = -99;
         
+        var scrapingTasks = new List<Task<Wr51CsvLine?>>();
+        var processCount = 1;
+        
         foreach (var filepath in files)
         {
-            var fileName = Path.GetFileName(filepath);
-
-            var fileId = FileHelper.ExtractFileId(fileName);
-            if (fileId == null)
-            {
-                ConsoleHelper.WriteLine($"ERROR - {fileName} doesn't contain a fileid guid");
-                continue;
-            }
+            Console.WriteLine($"Processing {filepath}");
             
-            var (_, alreadySaved, internalResults) = await GetMatchesAsync(
-                fileName,
-                fileId.Value,
-                lookupConfiguration,
-                pdfDataExtractor,
-                processRunId);
-
-            if (internalResults == null)
-            {
-                continue;
-            }
-            
-            if (alreadySaved == false)
-            {
-                await pdfDataExtractor.SaveMatchResultAsync(
-                    internalResults,
-                    fileId.Value,
+            scrapingTasks.Add(
+                ScrapeDocumentAsync(
+                    filepath,
+                    lookupConfiguration,
+                    pdfDataExtractor,
                     processRunId,
-                    lookupConfiguration.UseLockExclusivity);
-            }
+                    processCount++,
+                    files.Count));
             
-            var parsedForm = Wr51SchemaConverter.ToForm(internalResults);
-
-            if (parsedForm.Images.Count > 0)
-            {
-                var firstImage = parsedForm.Images.First();
-                var pathParts = firstImage.Split('/');
-                var folderName = pathParts[0];
+            await Task.WhenAny(scrapingTasks);
+            var toRemoveList = new List<Task<Wr51CsvLine?>>();
                 
-                Directory.CreateDirectory($"Images/{folderName}");
-
-                foreach (var image in parsedForm.Images)
+            foreach (var scrapingTask in scrapingTasks)
+            {
+                if (!scrapingTask.IsCompleted)
                 {
-                    var filenameParts = Path.GetFileNameWithoutExtension(image).Split('-');
-                    var serviceName = filenameParts[0];
-                    var pageNumber = int.Parse(filenameParts[1].Replace("page", string.Empty));
-                    var imageNumber = int.Parse(filenameParts[2].Replace("image", string.Empty));
+                    continue;
+                }
 
-                    var outputFolder = $"{parsedForm.Metadata.FileId}/{serviceName}/Images";
-                    var partialOutputFilename = $"page-{pageNumber}-image-{imageNumber}";
+                var result = scrapingTask.Result;
 
-                    var sourceImages = Directory.GetFiles($"Cache/{outputFolder}");
-                    var sourceImage = sourceImages
-                        .Select(Path.GetFileName)
-                        .Single(f => f!.StartsWith(partialOutputFilename, StringComparison.InvariantCultureIgnoreCase));
-
-                    var sourceFullPath = sourceImages.Single(i => i.Contains(sourceImage!, StringComparison.InvariantCultureIgnoreCase));
-                    var destinationFullPath = $"Images/{image}";
+                if (result != null)
+                {
+                    lines.Add(result);
+                }
                     
-                    File.Copy(sourceFullPath, destinationFullPath, true);
-                }
+                toRemoveList.Add(scrapingTask);
             }
 
-            var imagesSb = new StringBuilder();
-
-            foreach (var image in parsedForm.Images)
+            foreach (var toRemoveItem in toRemoveList)
             {
-                if (imagesSb.Length > 0)
-                {
-                    imagesSb.Append('\n');
-                }
-                
-                imagesSb.Append(image);
+                scrapingTasks.Remove(toRemoveItem);
             }
-            
-            lines.Add(new Wr51CsvLine
+        }
+        
+        foreach (var scrapingTask in scrapingTasks)
+        {
+            var result = scrapingTask.Result;
+
+            if (result != null)
             {
-                Metadata__Filename = parsedForm.Metadata.Filename,
-                Metadata__FormSentTo = parsedForm.Metadata.FormSentTo,
-                Metadata__DocumentTemplateVerison =  parsedForm.Metadata.DocumentTemplateVerison,
-                Metadata__IsScan =  parsedForm.Metadata.IsScan,
-                Metadata__Date__Date = parsedForm.Metadata.Date.Date?.ToString("dd/MM/yyyy"),
-                Metadata__Date__RawDate = parsedForm.Metadata.Date.RawDate,
-                LicenceNumber = parsedForm.LicenceNumber,
-                InspectionClass =  parsedForm.InspectionClass,
-                InspectingOfficer =   parsedForm.InspectingOfficer,
-                GeneralComments =  parsedForm.GeneralComments,
-                Images = imagesSb.Length > 0 ? imagesSb.ToString() : null,
-                Address__NameAndAddress = parsedForm.Address.NameAndAddress,
-                Address__SiteAddress = parsedForm.Address.SiteAddress,
-                Address__TelephoneNumber = parsedForm.Address.TelephoneNumber,
-                MetWith__Name = parsedForm.MetWith.Name,
-                MetWith__Position = parsedForm.MetWith.Position,
-                InspectionDate__DateTime = parsedForm.InspectionDate.DateTime?.ToString("dd/MM/yyyy HH:mm:ss"),
-                InspectionDate__RawDate = parsedForm.InspectionDate.RawDate,
-                InspectionDate__RawTime = parsedForm.InspectionDate.RawTime,
-                LicenceProvisions__SourceOfSupply = parsedForm.LicenceProvisions.SourceOfSupply.ToString(),
-                LicenceProvisions__Purposes = parsedForm.LicenceProvisions.Purposes.ToString(),
-                LicenceProvisions__PointOfAbstraction = parsedForm.LicenceProvisions.PointOfAbstraction.ToString(),
-                LicenceProvisions__SpecialConditions = parsedForm.LicenceProvisions.SpecialConditions.ToString(),
-                LicenceProvisions__MeansOfAbstraction = parsedForm.LicenceProvisions.MeansOfAbstraction.ToString(),
-                LicenceProvisions__Period = parsedForm.LicenceProvisions.Period.ToString(),
-                LicenceProvisions__Quantities = parsedForm.LicenceProvisions.Quantities.ToString(),
-                LicenceProvisions__MeansOfMeasurement = parsedForm.LicenceProvisions.MeansOfMeasurement.ToString(),
-                LicenceProvisions__Records = parsedForm.LicenceProvisions.Records.ToString(),
-                LicenceProvisions__ProvisionOfInformation = parsedForm.LicenceProvisions.ProvisionOfInformation.ToString(),
-                LicenceProvisions__Land = parsedForm.LicenceProvisions.Land.ToString(),
-                LicenceProvisions__ChargingFactors = parsedForm.LicenceProvisions.ChargingFactors.ToString(),
-                LicenceProvisions__OtherProvisions = parsedForm.LicenceProvisions.OtherProvisions.ToString(),
-                MeasurementDetails__MeterMake = parsedForm.MeasurementDetails.MeterMake,
-                MeasurementDetails__SerialNumber = parsedForm.MeasurementDetails.SerialNumber,
-                MeasurementDetails__Reading = parsedForm.MeasurementDetails.Reading,
-                MeasurementDetails__Units = parsedForm.MeasurementDetails.Units,
-                MeasurementDetails__Other = parsedForm.MeasurementDetails.Other,
-                MeasurementDetails__CertificatesOrRecordsAvailableFor = parsedForm.MeasurementDetails.CertificatesOrRecordsAvailableFor,
-                MeasurementDetails__DateOfCertificateOrRecord__Date = parsedForm.MeasurementDetails.DateOfCertificateOrRecord.Date?.ToString("dd/MM/yyyy"),
-                MeasurementDetails__DateOfCertificateOrRecord__RawDate = parsedForm.MeasurementDetails.DateOfCertificateOrRecord.RawDate,
-                MeasurementDetails__Calibration = parsedForm.MeasurementDetails.Calibration,
-                MeasurementDetails__Conformance = parsedForm.MeasurementDetails.Conformance,
-                MeasurementDetails__FlowVerification = parsedForm.MeasurementDetails.FlowVerification,
-                MeasurementDetails__MeterVerification = parsedForm.MeasurementDetails.MeterVerification,
-                MeasurementDetails__Maintenance__ByWhom = parsedForm.MeasurementDetails.Maintenance.ByWhom,
-                MeasurementDetails__Maintenance__Maintenance = parsedForm.MeasurementDetails.Maintenance.Maintenance,
-                MeasurementDetails__Maintenance__Frequency = parsedForm.MeasurementDetails.Maintenance.Frequency,
-                MeasurementDetails__ReadingsTaken__ByWhom = parsedForm.MeasurementDetails.ReadingsTaken.ByWhom,
-                MeasurementDetails__ReadingsTaken__ReadingsTaken = parsedForm.MeasurementDetails.ReadingsTaken.ReadingsTaken,
-                MeasurementDetails__ReadingsTaken__Frequency = parsedForm.MeasurementDetails.ReadingsTaken.Frequency,
-                MeasurementDetails__WhereKept = parsedForm.MeasurementDetails.WhereKept
-            });
+                lines.Add(result);
+            }
         }
 
+        pdfDataExtractor.Dispose();
+        
         var zipFileName = $"{uniqueFolder}-images.zip";
 
         if (File.Exists(zipFileName))
@@ -236,6 +159,147 @@ public static class GenerateWr51Csv
         ConsoleHelper.WriteLine("Finished generating wr51s csv");
         
         return 1;
+    }
+
+    private static async Task<Wr51CsvLine?> ScrapeDocumentAsync(
+        string filepath,
+        LookupConfiguration lookupConfiguration,
+        IPdfDataExtractorService pdfDataExtractor,
+        int processRunId,
+        int fileNumber,
+        int totalNumber)
+    {
+        var fileName = Path.GetFileName(filepath);
+
+        var dtStart = DateTime.Now;
+        ConsoleHelper.WriteLine($"INFO - {nameof(GenerateWr51Csv)}:{pdfDataExtractor.Id} - Started {fileName} ({fileNumber} of {totalNumber}) at {dtStart:yyyy-MM-dd HH:mm:ss}");
+        
+        var fileId = FileHelper.ExtractFileId(fileName);
+        if (fileId == null)
+        {
+            ConsoleHelper.WriteLine($"ERROR - {fileName} doesn't contain a fileid guid");
+            return null;
+        }
+        
+        var (_, alreadySaved, internalResults) = await GetMatchesAsync(
+            fileName,
+            fileId.Value,
+            lookupConfiguration,
+            pdfDataExtractor,
+            processRunId);
+
+        if (internalResults == null)
+        {
+            return null;
+        }
+        
+        if (alreadySaved == false)
+        {
+            await pdfDataExtractor.SaveMatchResultAsync(
+                internalResults,
+                fileId.Value,
+                processRunId,
+                lookupConfiguration.UseLockExclusivity);
+        }
+        
+        var parsedForm = Wr51SchemaConverter.ToForm(internalResults);
+
+        if (parsedForm.Images.Count > 0)
+        {
+            var firstImage = parsedForm.Images.First();
+            var pathParts = firstImage.Split('/');
+            var folderName = pathParts[0];
+            
+            Directory.CreateDirectory($"Images/{folderName}");
+
+            foreach (var image in parsedForm.Images)
+            {
+                var filenameParts = Path.GetFileNameWithoutExtension(image).Split('-');
+                var serviceName = filenameParts[0];
+                var pageNumber = int.Parse(filenameParts[1].Replace("page", string.Empty));
+                var imageNumber = int.Parse(filenameParts[2].Replace("image", string.Empty));
+
+                var outputFolder = $"{parsedForm.Metadata.FileId}/{serviceName}/Images";
+                var partialOutputFilename = $"page-{pageNumber}-image-{imageNumber}";
+
+                var sourceImages = Directory.GetFiles($"Cache/{outputFolder}");
+                var sourceImage = sourceImages
+                    .Select(Path.GetFileName)
+                    .Single(f => f!.StartsWith(partialOutputFilename, StringComparison.InvariantCultureIgnoreCase));
+
+                var sourceFullPath = sourceImages.Single(i => i.Contains(sourceImage!, StringComparison.InvariantCultureIgnoreCase));
+                var destinationFullPath = $"Images/{image}";
+                
+                File.Copy(sourceFullPath, destinationFullPath, true);
+            }
+        }
+
+        var imagesSb = new StringBuilder();
+
+        foreach (var image in parsedForm.Images)
+        {
+            if (imagesSb.Length > 0)
+            {
+                imagesSb.Append('\n');
+            }
+            
+            imagesSb.Append(image);
+        }
+        
+        return new Wr51CsvLine
+        {
+            Metadata__Filename = parsedForm.Metadata.Filename,
+            Metadata__FormSentTo = parsedForm.Metadata.FormSentTo,
+            Metadata__DocumentTemplateVerison =  parsedForm.Metadata.DocumentTemplateVerison,
+            Metadata__IsScan =  parsedForm.Metadata.IsScan,
+            Metadata__Date__Date = parsedForm.Metadata.Date.Date?.ToString("dd/MM/yyyy"),
+            Metadata__Date__RawDate = parsedForm.Metadata.Date.RawDate,
+            LicenceNumber = parsedForm.LicenceNumber,
+            InspectionClass =  parsedForm.InspectionClass,
+            InspectingOfficer =   parsedForm.InspectingOfficer,
+            GeneralComments =  parsedForm.GeneralComments,
+            Images = imagesSb.Length > 0 ? imagesSb.ToString() : null,
+            Address__NameAndAddress = parsedForm.Address.NameAndAddress,
+            Address__SiteAddress = parsedForm.Address.SiteAddress,
+            Address__TelephoneNumber = parsedForm.Address.TelephoneNumber,
+            MetWith__Name = parsedForm.MetWith.Name,
+            MetWith__Position = parsedForm.MetWith.Position,
+            InspectionDate__DateTime = parsedForm.InspectionDate.DateTime?.ToString("dd/MM/yyyy HH:mm:ss"),
+            InspectionDate__RawDate = parsedForm.InspectionDate.RawDate,
+            InspectionDate__RawTime = parsedForm.InspectionDate.RawTime,
+            LicenceProvisions__SourceOfSupply = parsedForm.LicenceProvisions.SourceOfSupply.ToString(),
+            LicenceProvisions__Purposes = parsedForm.LicenceProvisions.Purposes.ToString(),
+            LicenceProvisions__PointOfAbstraction = parsedForm.LicenceProvisions.PointOfAbstraction.ToString(),
+            LicenceProvisions__SpecialConditions = parsedForm.LicenceProvisions.SpecialConditions.ToString(),
+            LicenceProvisions__MeansOfAbstraction = parsedForm.LicenceProvisions.MeansOfAbstraction.ToString(),
+            LicenceProvisions__Period = parsedForm.LicenceProvisions.Period.ToString(),
+            LicenceProvisions__Quantities = parsedForm.LicenceProvisions.Quantities.ToString(),
+            LicenceProvisions__MeansOfMeasurement = parsedForm.LicenceProvisions.MeansOfMeasurement.ToString(),
+            LicenceProvisions__Records = parsedForm.LicenceProvisions.Records.ToString(),
+            LicenceProvisions__ProvisionOfInformation = parsedForm.LicenceProvisions.ProvisionOfInformation.ToString(),
+            LicenceProvisions__Land = parsedForm.LicenceProvisions.Land.ToString(),
+            LicenceProvisions__ChargingFactors = parsedForm.LicenceProvisions.ChargingFactors.ToString(),
+            LicenceProvisions__OtherProvisions = parsedForm.LicenceProvisions.OtherProvisions.ToString(),
+            MeasurementDetails__MeterMake = parsedForm.MeasurementDetails.MeterMake,
+            MeasurementDetails__SerialNumber = parsedForm.MeasurementDetails.SerialNumber,
+            MeasurementDetails__Reading = parsedForm.MeasurementDetails.Reading,
+            MeasurementDetails__Units = parsedForm.MeasurementDetails.Units,
+            MeasurementDetails__Other = parsedForm.MeasurementDetails.Other,
+            MeasurementDetails__CertificatesOrRecordsAvailableFor = parsedForm.MeasurementDetails.CertificatesOrRecordsAvailableFor,
+            MeasurementDetails__DateOfCertificateOrRecord__Date = parsedForm.MeasurementDetails.DateOfCertificateOrRecord.Date?.ToString("dd/MM/yyyy"),
+            MeasurementDetails__DateOfCertificateOrRecord__RawDate = parsedForm.MeasurementDetails.DateOfCertificateOrRecord.RawDate,
+            MeasurementDetails__Calibration = parsedForm.MeasurementDetails.Calibration,
+            MeasurementDetails__Conformance = parsedForm.MeasurementDetails.Conformance,
+            MeasurementDetails__FlowVerification = parsedForm.MeasurementDetails.FlowVerification,
+            MeasurementDetails__MeterVerification = parsedForm.MeasurementDetails.MeterVerification,
+            MeasurementDetails__Maintenance__ByWhom = parsedForm.MeasurementDetails.Maintenance.ByWhom,
+            MeasurementDetails__Maintenance__Maintenance = parsedForm.MeasurementDetails.Maintenance.Maintenance,
+            MeasurementDetails__Maintenance__Frequency = parsedForm.MeasurementDetails.Maintenance.Frequency,
+            MeasurementDetails__ReadingsTaken__ByWhom = parsedForm.MeasurementDetails.ReadingsTaken.ByWhom,
+            MeasurementDetails__ReadingsTaken__ReadingsTaken = parsedForm.MeasurementDetails.ReadingsTaken.ReadingsTaken,
+            MeasurementDetails__ReadingsTaken__Frequency = parsedForm.MeasurementDetails.ReadingsTaken.Frequency,
+            MeasurementDetails__WhereKept = parsedForm.MeasurementDetails.WhereKept
+        };
     }
     
     private static Task<(bool StopExecution, bool? AlreadySaved, MatchesResult? Item)> GetMatchesAsync(
@@ -267,6 +331,6 @@ public static class GenerateWr51Csv
             GeneralConstants.UnsetRegionCode,
             DateTime.Now,
             lineHeight: 6,
-            minimumRowsForDigital: 40);
+            minimumRowsForDigital: 30);
     }
 }

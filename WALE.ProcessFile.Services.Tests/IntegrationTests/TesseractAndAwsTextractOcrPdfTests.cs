6b39168f9dd814436731a9c2df76c5a45ae52afb
Ryan Barlow <ryan.barlow@environment-agency.gov.uk>
2026-07-24 14:38:02 +0100

    Better finding of sub sections for abstraction limits

--- WALE.ProcessFile.Services.Tests/IntegrationTests/TesseractAndAwsTextractOcrPdfTests.cs
+++ WALE.ProcessFile.Services.Tests/IntegrationTests/TesseractAndAwsTextractOcrPdfTests.cs
@@ -157,7 +157,7 @@ public class TesseractAndAwsTextractOcrPdfTests(SingletonAwsTextractFixture text
         Assert.Single(abstractionLimitsSection.SubResults);
         var section1Sub1 = abstractionLimitsSection.SubResults![0];
         
-        Assert.Equal(4, section1Sub1.SubResults.Count);
+        Assert.Equal(5, section1Sub1.SubResults.Count);
         // TODO fix for this
         
         var perDayUnits = section1Sub1.SubResults?.FirstOrDefault(x => x.MatchedLabel!.Name == "PerDayUnits");

25ad8c3668c94f4eaf20d32f54d4069c19e96e6c
James Bayliss Dawson <james.baylissdawson@environment-agency.gov.uk>
2026-08-17 11:14:47 +0100

    242 - Only show the most recent BR tag

--- WRADI.Core.AbstractionLicence/Strategies/LinkedLicencesVerificationOutputStrategy.cs
+++ WRADI.Core.AbstractionLicence/Strategies/LinkedLicencesVerificationOutputStrategy.cs
@@ -54,7 +54,10 @@ public class LinkedLicencesVerificationOutputStrategy : IVerificationOutputStrat
 
     private static bool IsAutoOrBusinessReview(string? verificationType)
         => verificationType is "AutoWarn" or "AutoFail"
-            or "RequestBusinessReview" or "CompleteBusinessReview";
+           || IsBusinessReview(verificationType);
+
+    private static bool IsBusinessReview(string? verificationType)
+        => verificationType is "RequestBusinessReview" or "CompleteBusinessReview";
 
     private static void ProcessOutgoingVerifications(IEnumerable<LicenceSectionVerification> verifications,
         OutputListDataItem listRow, List<LicenceSectionItemSummary> sectionSummaries,
@@ -203,6 +206,14 @@ public class LinkedLicencesVerificationOutputStrategy : IVerificationOutputStrat
         }
         else
         {
+            // New business review tags should override previous ones - clear the previous ones first
+            if (IsBusinessReview(verification.VerificationType))
+            {
+                existingSummary.VerificationTypes = existingSummary.VerificationTypes
+                    .Where(x => !IsBusinessReview(x))
+                    .ToArray();
+            }
+            
             existingSummary.CurrentVerificationType = verification.VerificationType!;
             if (!existingSummary.VerificationTypes.Contains(verification.VerificationType!))
             {

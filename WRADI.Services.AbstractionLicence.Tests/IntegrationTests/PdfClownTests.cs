9d6494ffff5075f24227bcb32e384700d75ab970
Ryan Barlow <ryan.barlow@environment-agency.gov.uk>
2026-09-17 11:10:03 +0100

    ...

--- WRADI.Services.AbstractionLicence.Tests/IntegrationTests/PdfClownTests.cs
+++ WRADI.Services.AbstractionLicence.Tests/IntegrationTests/PdfClownTests.cs
@@ -1,4 +1,10 @@
 using iTextSharp.text.pdf;
+using KillerPdf.Engine.Authoring;
+using org.pdfclown.documents.contents.objects;
+using org.pdfclown.files;
+using org.pdfclown.util.collections.generic;
+using File = System.IO.File;
+using Path = org.pdfclown.documents.contents.objects.Path;
 
 namespace WRADI.Services.AbstractionLicence.Tests.IntegrationTests;
 
@@ -25,14 +31,72 @@ public class PdfClownTests
         using var pdf = new org.pdfclown.files.File("output4.pdf");
         var doc = pdf.Document;
 
+        ContentObject? updatedContentObject = null;
+        var removePos = -1;
+        
         foreach (var page in doc.Pages)
         {
-            foreach (var a in page.Resources.Fonts.Keys)
+            var idx = 0;
+            
+            foreach (var content in page.Contents)
             {
-                Console.WriteLine(page.Resources.Fonts[a].Name);
+                if (content is Text text)
+                {
+                    foreach (var subContent in text.Objects)
+                    {
+                        if (subContent is ShowSimpleText showSimpleText)
+                        {
+                            var existingValue = (byte[])showSimpleText.Value[0];
+
+                            if (existingValue.Length == 8 && existingValue[0] == 65)
+                            {
+                                // Seem to be limited by what characters are available or registered in the font/glyph
+                                existingValue[0] = (byte)'A';
+                                existingValue[1] = (byte)'B';
+                                existingValue[2] = (byte)'C';
+                                existingValue[3] = (byte)' ';
+                                existingValue[4] = (byte)' ';
+                                existingValue[5] = (byte)' ';
+                                existingValue[6] = (byte)' ';
+                                existingValue[7] = (byte)' ';                                
+
+                                removePos = idx;
+                                updatedContentObject = content;
+                            }
+                        }
+                        
+                        /*if (subContent is ShowText showText)
+                        {
+                           
+                        }*/
+                    }
+                }
+                
+                /*if (content is Path path)
+                {
+                    foreach (var subContent in path.Objects)
+                    {
+                        if (subContent is MarkedContent markedContent)
+                        {
+                            
+                        }
+                    }
+                }*/
+
+                idx += 1;
+            }
+
+            if (page.Index == 0)
+            {
+                var contents = page.Contents;
+                contents.RemoveAt(removePos);
+                contents.Insert(removePos, updatedContentObject);
+                contents.Flush();
             }
         }
-        
+
+        pdf.Save("Test1.pdf", SerializationModeEnum.Standard);
+
         // Act
 
         // Assert

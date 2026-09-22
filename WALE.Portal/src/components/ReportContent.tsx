bb3924de3e6318d3da672f19ae1c85bc80062ee0
Ryan Barlow <ryan-barlow@users.noreply.github.com>
2026-09-22 14:01:02 +0100

    WRADI-265 - Fix refresh bug (#293)
    Co-authored-by: Ryan Barlow <ryan.barlow@environment-agency.gov.uk>

--- WALE.Portal/src/components/ReportContent.tsx
+++ WALE.Portal/src/components/ReportContent.tsx
@@ -45,7 +45,6 @@ export function ReportContent({ fileId, licenceId, matchesResultId, hideBackLink
     const loadAllData = async () => {
         try {
             setLoading(true);
-            debugger;
             
             // Load data using API client
             const [matchesResult, matchesResultString, licenceResult, currentLicenceResult, licenceSetsResult, licenceStringResult, licenceVerificationStringResult] = await Promise.allSettled([
